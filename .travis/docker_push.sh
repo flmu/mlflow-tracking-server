7855b2bcb829ea2e06b60e17b38103cd7927fecd
Florian Muchow <florian.muchow@sap.com>
2018-10-01 09:32:00 +0200

    improved travis

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build --no-cache -t mlflow-tracking-server:0.1.0 .
 docker tag mlflow-tracking-server:0.1.0 foxrider/mlflow-tracking-server:0.1.0
 docker push mlflow-tracking-server:0.1.0
---
+++ .travis/docker_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build --no-cache -t mlflow-tracking-server:0.1.0 .
