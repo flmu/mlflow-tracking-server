93cb9c90f1c89ad1a9631a04b69149f172450a54
Florian Muchow <30693550+flmu@users.noreply.github.com>
2019-08-14 20:18:09 +0200

    CleanUp Code + Documentation (#8)
    * added architecture picture to Readme
    
    * added architecture picture to Readme
    
    * clean up

--- mlflow_client_examples/test.py
+++ mlflow_client_examples/test.py
@@ -5,18 +5,23 @@ import mlflow
 if __name__ == "__main__":
     print("Running the test script ...")
 
+    #Connect to tracking server
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
+    #Create directory for artifacts
     if not os.path.exists("artifact_folder"):
         os.makedirs("artifact_folder")
 
+    #Test parametes
     mlflow.log_param("param1", randint(0, 100))
 
-    mlflow.log_metric("foo", random())
-    mlflow.log_metric("foo", random() + 1)
-    mlflow.log_metric("foo", random() + 2)
+    #Test metrics
+    mlflow.log_metric("metric1", random())
+    mlflow.log_metric("metric1", random())
+    mlflow.log_metric("metric1", random())
+    mlflow.log_metric("metric1", random())
 
+    #Test artifact
     with open("artifact_folder/test.txt", "w") as f:
         f.write("hello world!")
-
     mlflow.log_artifacts("artifact_folder")
