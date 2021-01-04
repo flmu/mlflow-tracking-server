4703fb7974a09fb2073ce3a3debb280a56b4a745
Eric Zhang <ekzhang1@gmail.com>
2021-01-04 12:44:55 -0600

    Update to latest MLFlow and Python versions
    - Update MLFlow 1.3 -> MLFlow 1.13
    - Change internal file system store to SQLite
      (file system URIs are unsupported for model artifact tracking stores)
    - Update Python 3.7 -> Python 3.9
    - Update boto3 1.9.240 -> boto3 1.16.46
    - Remove some unused defaults in Dockerfile

--- mlflow_tracking_server/files/run.sh
+++ mlflow_tracking_server/files/run.sh
@@ -12,18 +12,8 @@ if [ -z $AWS_BUCKET ]; then
   exit 1
 fi
 
-if [ -z $AWS_ACCESS_KEY_ID ]; then
-  echo >&2 "AWS_ACCESS_KEY_ID must be set"
-  exit 1
-fi
-
-if [ -z $AWS_SECRET_ACCESS_KEY ]; then
-  echo >&2 "AWS_SECRET_ACCESS_KEY must be set"
-  exit 1
-fi
-
 mkdir -p $FILE_DIR && mlflow server \
-    --backend-store-uri file://${FILE_DIR} \
+    --backend-store-uri sqlite:///${FILE_DIR}/sqlite.db \
     --default-artifact-root s3://${AWS_BUCKET}/artifacts \
     --host 0.0.0.0 \
     --port $PORT
