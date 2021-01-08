2bb1b8b174d4a1b65b54c5b29faa692f142b2f4d
Florian Muchow <30693550+flmu@users.noreply.github.com>
2021-01-08 08:44:00 +0100

    Remove AWS Null Checks + Version Bump (#21)
    * version bump
    
    * remove null checks

--- .travis/docker_script.sh
+++ .travis/docker_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd mlflow_tracking_server && docker build --no-cache -t mlflow-tracking-server:0.4.0 .
+cd mlflow_tracking_server && docker build --no-cache -t mlflow-tracking-server:0.5.0 .
