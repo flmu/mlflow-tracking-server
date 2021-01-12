363e9b9e4f4123b3edd610e6ab6c45c55946f302
flmu <flmuchow@gmail.com>
2021-01-12 17:46:15 +0100

    address code-inspector issues

--- mlflow_tracking_server/files/run.sh
+++ mlflow_tracking_server/files/run.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-if [ -z $FILE_DIR ]; then
+if [ -z "$FILE_DIR" ]; then
   echo >&2 "FILE_DIR must be set"
   exit 1
 fi
 
-if [ -z $AWS_BUCKET ]; then
+if [ -z "$AWS_BUCKET" ]; then
   echo >&2 "AWS_BUCKET must be set"
   exit 1
 fi
 
-mkdir -p $FILE_DIR && mlflow server \
+mkdir -p "$FILE_DIR" && mlflow server \
     --backend-store-uri sqlite:///${FILE_DIR}/sqlite.db \
     --default-artifact-root s3://${AWS_BUCKET}/artifacts \
     --host 0.0.0.0 \
